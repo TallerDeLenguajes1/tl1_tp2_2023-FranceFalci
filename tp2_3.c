80b8322b7cfded05f0346a1c5b970496d500f65b
Francesca Falci <francescafacia@gmail.com>
2023-03-24 18:27:49 -0300

    punto 3

--- tp2_3.c
+++ tp2_3.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#define N 5
 #define M 7
+#define N 5
 
-
-int main(){
-
-int i,j;
-int mt[N][M];
-int *pmt = mt;
-srand(time(NULL));
-
-for(i = 0;i<N; i++)
-{
-for(j = 0;j<M; j++)
-{
-pmt[i][j]=1+rand()%100;
-printf("%lf" , *pmt);
-}
-printf("\n");
-}
-
+int main() {
+    double matrix[N][M];
+    srand(time(NULL));
+    for(int i = 0; i < N; i++) {
+        printf("\n");
+        for(int j = 0; j < M; j++) {
+            *(*(matrix + i) + j) = rand()%100 + 1;
+            printf("%2.lf    ",*(*(matrix + i) + j));
+        }
+    }
+    return 0;
 }
