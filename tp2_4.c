b04db6c0cf8a4cd52a566cd83935df5874031ea0
Francesca Falci <francescafacia@gmail.com>
2023-03-24 18:30:14 -0300

    empezando punto 4

---
+++ tp2_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct compu {
+    int velocidad;//entre 1 y 3 GHz
+    int anio;//entre 2015 y 2023
+    int cantidad;//entre 1 y 8
+    char *tipo_cpu; //valores del arreglo tipos
+};
+int main(){
+
+    return 0;
+}
