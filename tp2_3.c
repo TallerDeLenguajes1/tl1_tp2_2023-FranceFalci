1b7d6cfdd63aa190090e472012fbf73552b9760b
France <francescafacia@gmail.com>
2023-03-28 16:00:35 -0300

    terminando apartado 4.b

--- tp2_3.c
+++ tp2_3.c
@@ -1,18 +1,28 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define M 7
-#define N 5
 
-int main() {
-    double matrix[N][M];
-    srand(time(NULL));
-    for(int i = 0; i < N; i++) {
-        printf("\n");
-        for(int j = 0; j < M; j++) {
-            *(*(matrix + i) + j) = rand()%100 + 1;
-            printf("%2.lf    ",*(*(matrix + i) + j));
+#define N 5
+#define M 7
+
+int main()
+{
+    printf("hola");
+    int i, j;
+    int mt[N][M];
+    int *p = &mt[0][0];
+    srand(time(NULL)); // inicializa la semilla del generador de números aleatorios
+
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            *(p + i * M + j) = 1 + rand() % 100;
+            printf("%d ", *(p + i * M + j));
         }
+        printf("\n");
     }
+
     return 0;
 }
